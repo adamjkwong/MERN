a7aa9e9ac9eb4cf3d8752dcf2e0e170a770c0505
adamjkwong <adamjkwong@gmail.com>
2021-04-09 09:28:50 -0700

    JSX homework completed!

---
+++ homework/prop_it_up/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import MyNewComponent from './components/MyNewComponent';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNewComponent someText={"Welcome to our roster page!"}/>
+      <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+      <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={52} hairColor={"Brown"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ homework/prop_it_up/src/components/MyNewComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MyNewComponent extends Component{
+    render(){
+        return(
+            <div>
+                {this.props.someText}
+            </div>
+        )
+    }
+}
+export default MyNewComponent
