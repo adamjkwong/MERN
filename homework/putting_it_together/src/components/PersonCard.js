f4c928e0f30ecce146eb3539652afc58b72c462a
adamjkwong <adamjkwong@gmail.com>
2021-04-10 22:36:37 -0700

    putting_it_together

--- homework/putting_it_together/src/components/PersonCard.js
+++ homework/putting_it_together/src/components/PersonCard.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            agePlus: this.props.age,
+        };
+    }
     render(){
         return(
             <div class="center-it">
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
                 <p>
-                    Age: {this.props.age}
+                    Age: {this.state.agePlus}
                 </p>
                 <p>
                     Hair Color: {this.props.hairColor}
                 </p>
+                <button onClick={()=> this.setState({ agePlus: this.state.agePlus +1 })}>
+                    Birthday Button for {this.props.firstName} {this.props.lastName}
+                </button>
             </div>
         )
     }
