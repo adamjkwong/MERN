ee36b18f331856f4a29992ee2e3dfe4441b30e67
adamjkwong <adamjkwong@gmail.com>
2021-04-25 12:28:52 -0700

    latest

--- homework/tabs/src/App.js
+++ homework/tabs/src/App.js
@@ -4,12 +4,17 @@ import TabTop from './components/TabTop';
     
     
 function App() {
-    const [message, setMessage ] = useState([]);
-  
+    const [message, setMessage ] = useState("");
+
     return (
-        <div className="App">
+        <div className="AllContainer">
             <h1>Tabs Assignment</h1>
-            <TabTop>
+            <div className="TabContainer">
+                <TabTop id="1" className="tab" tabNum={1} message={ message } setMessage={ setMessage }/>
+                <TabTop id="2" className="tab" tabNum={2} message={ message } setMessage={ setMessage }/>
+                <TabTop id="3" className="tab" tabNum={3} message={ message } setMessage={ setMessage }/>
+                <p> { message }</p>
+            </div>
         </div>
     );
 }
--- homework/tabs/src/components/TabTop.jsx
+++ homework/tabs/src/components/TabTop.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Tabtop = props => {
-    const onClickHandler = (e, value) => {
-        alert(value);
+const TabTop = props => {
+    const [tabNum] = useState("");
+    const [message, setMessage ] = useState("");
+    const onClickHandler = (e) => {
+        e.preventDefault();
+        alert("You have clicked the button");
+        setMessage ("The selected tab is: " + props.tabNum) ;
     }
+// var handleTab = (e) => {
+//     setFirstName(e.target.value)
+//     if(firstName.length == 0) {
+//         setFirstNameError("");
+//     } else if(firstName.length < 5) {
+//         setFirstNameError("First name must be at least 5 characters!");
+//     } else {
+//         setFirstNameError("");
+//     }
+// }
  
-    return props.movies.map( (item, index) => {
-        return <button onClick={ (e) => onClickHandler(e, item) }>{ item }</button>
-    });
+    return (
+        <div>
+            <p class = "TabTopper" onClick={ onClickHandler }>Tab Num: { props.tabNum } </p>
+        </div>
+    );
+}
     
 export default TabTop;
