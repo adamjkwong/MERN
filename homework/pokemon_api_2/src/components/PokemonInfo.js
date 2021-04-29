c16b571b0fd534a0d657eb04011831c8dffbf180
adamjkwong <adamjkwong@gmail.com>
2021-04-28 17:19:48 -0700

    pokemon some edits

--- homework/pokemon_api_2/src/components/PokemonInfo.js
+++ homework/pokemon_api_2/src/components/PokemonInfo.js
@@ -7,25 +7,25 @@ function App() {
 
   useEffect(() => {
     axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
-      .then(response => {
-        setPokemon(response.data.results)
-      })
-      .catch((err) => {
-        console.log(err);
-      })
+        .then(response => {
+            setPokemon(response.data.results)
+    })
+        .catch((err) => {
+            console.log(err);
+    })
     
-  }, []);  
-  return (
-    <div className="App" style={{ width: "200px", margin: "auto" }}>
-      <ul>
-        {
-          pokemons.map((pokemonObj, index)=>{
-            return (<li key={index}>{pokemonObj.name}</li>)
-          })
-        }
-      </ul>
+}, []);  
+    return (
+        <div className="App" style={{ width: "200px", margin: "auto" }}>
+        <ul>
+            {
+            pokemons.map((pokemonObj, index)=>{
+                return (<li key={index}>{pokemonObj.name}</li>)
+            })
+            }
+        </ul>
     </div>
-  );
+);
 }
 
 export default App;
